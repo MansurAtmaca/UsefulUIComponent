5c63a44d4e41222a9f2e6ebc27c4b036b3bd642c
MansurcanAtmaca <mansur-atmaca@hotmail.com>
2024-02-10 12:29:48 +0300

    added ımageCard

--- app/src/main/java/com/example/usefuluicomponents/ImageCard.kt
+++ app/src/main/java/com/example/usefuluicomponents/ImageCard.kt
@@ -1,16 +1,79 @@
-package com.example.usefuluicomponents.ui.theme
+package com.example.usefuluicomponents
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.usefuluicomponents.ui.theme.UsefulUIComponentsTheme
 
 @Composable
 fun ImageCard(
     painter:Painter,
     contentDescriptor:String,
-    title:String
+    title:String,
+    modifier: Modifier = Modifier
 ){
-    Box(modifier = Modifier.fillMaxWidth()){}
+    Card(
+        modifier = modifier.fillMaxWidth()
+            ,
+        shape = RoundedCornerShape(16.dp))
+    {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter, 
+                contentDescription = contentDescriptor,
+                contentScale = ContentScale.FillBounds
+
+            )
+            Box(modifier = Modifier.fillMaxSize()
+                .background(Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black
+                    ),
+                    startY = 300f
+                )))
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+                contentAlignment = Alignment.BottomStart
+            ){
+                Text(
+                    text = title,
+                    fontSize = 16.sp,
+                    color = Color.White)
+            }
+        }
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    UsefulUIComponentsTheme {
+        val paintName = painterResource(id = R.drawable.archer)
+        Box(modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(8.dp)){
+            ImageCard(painter = paintName, contentDescriptor = "archer", title =   "Archer")
+        }
+
+    }
 }
--- app/src/main/java/com/example/usefuluicomponents/MainActivity.kt
+++ app/src/main/java/com/example/usefuluicomponents/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.usefuluicomponents.ui.theme.UsefulUIComponentsTheme
 
@@ -22,25 +23,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UsefulUIComponentsTheme {
-        Greeting("Android")
-    }
-}
+
