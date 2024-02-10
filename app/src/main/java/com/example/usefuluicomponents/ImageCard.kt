4ea28b07657b8c9d395c24daf6011afff3764210
MansurcanAtmaca <mansur-atmaca@hotmail.com>
2024-02-10 12:28:11 +0300

    added ImageCar

---
+++ app/src/main/java/com/example/usefuluicomponents/ImageCard.kt
@@ -0,0 +1,16 @@
+package com.example.usefuluicomponents.ui.theme
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+
+@Composable
+fun ImageCard(
+    painter:Painter,
+    contentDescriptor:String,
+    title:String
+){
+    Box(modifier = Modifier.fillMaxWidth()){}
+}
